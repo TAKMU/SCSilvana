29dfd31120923686fe99ecbcf354ef5fc9f626b1
TAKMU <allan.miyazono@gmail.com>
2025-04-07 16:09:38 -0600

    Added function to download X

--- download_pooch.py
+++ download_pooch.py
@@ -7,6 +7,8 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
+import scipy.sparse as sparse
+from scipy import io
 
 dir = f"./data/"
 if not os.path.exists(dir):
@@ -22,9 +24,9 @@ file_path = pooch.retrieve(
 )
 
 data_raw_path = "./data/whole_taxonomy_MTG_AD.h5ad"
+batch_size = 50000
 adata = sc.read_h5ad(data_raw_path, backed="r")
 
-batch_size = 50000
 dict_types ={
     "sc" : "10x 3' v3",
     "sn" : "10x multiome"
@@ -69,6 +71,64 @@ def filtered_qc(path, fields):
     )
     return adata.obs[fields].copy()
 
+def get_batch_X(i, batch_size, path):
+    adata = sc.read_h5ad(path, backed="r", as_sparse_fmt = sparse._csr.csr_matrix)
+    batch = adata[i:i + batch_size, :].to_memory()
+    batch_X =  batch.X.astype("float32")
+    io.mmwrite(f"./data/chunk_{i}.mtx", batch_X)
+
+
+def get_batch_cell_id(i, batch_size, path):
+    adata = sc.read_h5ad(path, backed="r", as_sparse_fmt = sparse._csr.csr_matrix)
+    batch = adata[i:i + batch_size, :].to_memory()
+    cell_ids_batch = batch.obs_names.to_frame(name="cell_id")
+    return cell_ids_batch
+
+
+def get_batch_gene_id(i, batch_size, path):
+    adata = sc.read_h5ad(path, backed="r", as_sparse_fmt = sparse._csr.csr_matrix)
+    batch = adata[i:i + batch_size, :].to_memory()
+    gene_ids_batch = batch.var_names.to_frame(name="gene_id")
+    return gene_ids_batch
+
+def get_batch_metadata(i, batch_size, path):
+    adata = sc.read_h5ad(path, backed="r", as_sparse_fmt = sparse._csr.csr_matrix)
+    batch = adata[i:i + batch_size, :].to_memory()
+    metadata_batch = batch.obs
+    return metadata_batch
+
+Parallel(n_jobs=4)(
+    delayed(get_batch_X)(i, batch_size, data_raw_path)
+    for i in range(0, adata.n_obs, batch_size)
+)
+chunks = sorted(glob.glob(f"./data/chunk_*.mtx"))
+X_final = sparse.vstack([io.mmread(chunk) for chunk in chunks])
+io.mmwrite("./data/matriz_procesada_completa.mtx", X_final)
+
+batch_size = 50000
+results = Parallel(n_jobs=4)(
+    delayed(get_batch_cell_id)(i, batch_size, data_raw_path)
+    for i in range(0, adata.n_obs, batch_size)
+)
+cell_ids = pd.concat(results)
+
+results = Parallel(n_jobs=4)(
+    delayed(get_batch_gene_id)(i, batch_size, data_raw_path)
+    for i in range(0, adata.n_obs, batch_size)
+)
+gene_ids = pd.concat(results)
+
+results = Parallel(n_jobs=4)(
+    delayed(get_batch_metadata)(i, batch_size, data_raw_path)
+    for i in range(0, adata.n_obs, batch_size)
+)
+metadata_cells = pd.concat(results)
+
+
+cell_ids.to_csv("./data/cell_ids.csv", index=False)
+gene_ids.to_csv("./data/gene_ids.csv", index=False)
+metadata_cells.to_csv("./data/metadata_cells.csv")
+
 
 
 #Parallel(n_jobs=4)(
@@ -76,53 +136,35 @@ def filtered_qc(path, fields):
 #    for i in range(0, adata.n_obs, batch_size)
 #)
 
-for key in dict_types.keys():
-    all_files = glob.glob(f"./data/{key}/*.h5ad")
-    results = Parallel(n_jobs = 4)(
-        delayed(filtered_qc)(file, fields)
-        for file in all_files    
-    )
-    all_obs = pd.concat(results, ignore_index=True)
-    melted = pd.melt(
-        all_obs,
-        value_vars=fields,
-        var_name="feature",
-        value_name="value"
-    )
-    for feature_name in melted["feature"].unique():
-        feature_data = melted[melted["feature"] == feature_name]
-    
-        plt.figure(figsize=(6, 4))
-        sns.violinplot(data=feature_data, x="feature", y="value", inner="box", density_norm='width')
-        sns.stripplot(data=feature_data, x="feature", y="value", color="k", jitter=0.4, size=0.1)
-        plt.title(f"Violin plot for {feature_name}")
-        plt.ylabel("Value")
-        plt.xlabel("")
-        plt.tight_layout()
-        plt.show()
+#for key in dict_types.keys():
+#    all_files = glob.glob(f"./data/{key}/*.h5ad")
+#    results = Parallel(n_jobs = 4)(
+#        delayed(filtered_qc)(file, fields)
+#        for file in all_files    
+#    )
+#    all_obs = pd.concat(results, ignore_index=True)
+#    melted = pd.melt(
+#        all_obs,
+#        value_vars=fields,
+#        var_name="feature",
+#        value_name="value"
+#    )
+#    for feature_name in melted["feature"].unique():
+#        feature_data = melted[melted["feature"] == feature_name]
+#    
+#        plt.figure(figsize=(6, 4))
+#        sns.violinplot(data=feature_data, x="feature", y="value", inner="box", density_norm='width')
+#        sns.stripplot(data=feature_data, x="feature", y="value", color="k", jitter=0.4, size=0.1)
+#        plt.title(f"Violin plot for {feature_name}")
+#        plt.ylabel("Value")
+#        plt.xlabel("")
+#        plt.tight_layout()
+#        plt.show()
     #g = sns.stripplot(data=melted, x="feature", y="value", color="k", alpha=0.3, jitter=0.4)
     #plt.show()
 
 
-def obtain_matrix(path):
-    adata = sc.read_h5ad(path)
-    return adata.X
-
-for key in dict_types.keys():
-    all_files = glob.glob(f"./data/{key}/*.h5ad")
-    results = Parallel(n_jobs = 4)(
-        delayed(obtain_matrix)(file)
-        for file in all_files    
-    )
-    matrix = np.concat(results, ignore_index=True)
 
 
 
 
-## Split the dataset into scRNA-seq and snRNA-seq
-#adata_scrna = adata_sample[adata_sample.obs["assay"] == "10x 3' v3"].copy()
-#adata_snrna = adata_sample[adata_sample.obs["assay"] == "10x multiome"].copy()
-#
-## Verify the number of cells in each dataset
-#print(f"scRNA-seq: {adata_scrna.n_obs} cells")
-#print(f"snRNA-seq: {adata_snrna.n_obs} nuclei")
